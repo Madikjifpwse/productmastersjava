f7ae2fef92f1b9738bc17750eaf177f2f3436bd4
Madiyar Madikhan <madiyrmadihan@gmail.com>
2025-02-19 13:14:27 +0500

    .

--- Module 4/Homework/Hard/src/Main.java
+++ Module 4/Homework/Hard/src/Main.java
@@ -3,6 +3,8 @@ public class Main {
         Box<Integer> intBox = new Box<>(100);
         System.out.println("Содержимое: " + intBox.getItem());
         intBox.showType();
+        intBox.setItem(200);
+        System.out.println("Новое содержимое: " + intBox.getItem());
         Box<String> strBox = new Box<>("Hello world!");
         System.out.println("Содержимое: " + strBox.getItem());
         strBox.showType();
